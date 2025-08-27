f9923f8d608d5f323643272defdfad67a20817cb
SampadaPawate <sampadapawate@gmail.com>
2025-08-27 15:00:48 -0500

    favicon change

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,15 +59,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
-    other: [
-      { rel: 'mask-icon', url: '/safari-pinned-tab.svg', color: '#000000' },
+      { url: '/uta-favicon.ico' },
     ],
   },
   manifest: '/site.webmanifest',
