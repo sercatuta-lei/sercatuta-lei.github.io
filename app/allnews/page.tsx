c341506d78f8e3adba7c4a14fcf38d46f0998702
sercatuta-lei <126905106+sercatuta-lei@users.noreply.github.com>
2025-12-04 10:38:12 -0600

    Update news date and conference name for Krishna

--- app/allnews/page.tsx
+++ app/allnews/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 const news = [
   {
-  date: "2025",
-  headline: "Congratulations to Krishna for his paper titled 'ABLE: Using Adversarial Pairs to Construct Local Models for Explaining Model Predictions', which has been accepted by KDD 2026!",
+  date: "November 2025",
+  headline: "Congratulations to Krishna for his paper titled 'ABLE: Using Adversarial Pairs to Construct Local Models for Explaining Model Predictions', which has been accepted by ACM SIGKDD 2026!",
   },
   {
     date: "July 14, 2025",
