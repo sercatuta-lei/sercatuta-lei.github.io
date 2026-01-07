44b4684de71e52495425843bf7d77f5408314396
sercatuta-lei <126905106+sercatuta-lei@users.noreply.github.com>
2026-01-07 16:58:35 -0600

    Add news entry for Krishna's published paper

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,10 @@ import Link from "next/link";
 // News data
 const newsData = [
   {
+  date: "January 2026",
+  headline:"Congratulations to Krishna for his paper titled 'A Combinatorial Approach to Synthetic Data Generation for Machine Learning' that has been published in Springer Nature Computer Science.",
+  },
+  {
   date: "November 2025",
   headline: "Congratulations to Krishna for his paper titled 'ABLE: Using Adversarial Pairs to Construct Local Models for Explaining Model Predictions', which has been accepted by ACM SIGKDD 2026!",
   },
